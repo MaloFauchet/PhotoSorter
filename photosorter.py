57b4343bcbb16817e879efd7d078320241f9fbb0
FAUCHET Malo <malofauchetpro@gmail.com>
2024-01-31 11:34:20 +0100

    avancement GUI

--- photosorter.py
+++ photosorter.py
@@ -3,8 +3,7 @@ from sorter import Sorter
 
 # used for gui
 import customtkinter as cs
-
-from time import time
+from tkinter import filedialog
 
 
 class Photosorter(cs.CTk):
@@ -21,9 +20,20 @@ class Photosorter(cs.CTk):
         # Labels
         self.title_label: cs.CTkLabel = cs.CTkLabel(self)
         self.explaination_label: cs.CTkLabel = cs.CTkLabel(self)
+        self.sort_label: cs.CTkLabel = cs.CTkLabel(self)
+        self.help_sort_label: cs.CTkLabel = cs.CTkLabel(self)
+        self.warning_label: cs.CTkLabel = cs.CTkLabel(self)
 
         # Entries
         self.old_img_dir_entry: cs.CTkEntry = cs.CTkEntry(self)
+        self.sort_entry: cs.CTkEntry = cs.CTkEntry(self)
+
+        # Buttons
+        self.old_img_dir_btn: cs.CTkButton = cs.CTkButton(self)
+        self.sort_help_button: cs.CTkButton = cs.CTkButton(self)
+
+        # Algo variables
+        self.help_is_displayed = False
 
         # draw(?) the main window
         self.main_window()
@@ -33,25 +43,120 @@ class Photosorter(cs.CTk):
         # configure the grid
         for i in range(10):
             self.rowconfigure(i, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=0)
 
         # title
         self.title_label = cs.CTkLabel(
             self,
-            text="Bienvenue dans le trieur de photo",
+            text="Welcome in the PhotoSorter",
             font=("Arial", 20, 'bold'),
-            height=2,
-            # sticky="news"
+            height=2
         )
-        self.title_label.grid(row=0, column=0)
+        self.title_label.grid(row=0, column=0, sticky="ew")
 
         # explaination of the entry below it (old_img_dir)
         self.explaination_label = cs.CTkLabel(
             self,
-            text="Mettre ici le chemin de vos photos",
+            text="Put here the path to you images",
             font=("Arial", 15, 'bold'),
             height=2
         )
         self.explaination_label.grid(row=1, column=0)
+
+        # manual entry of the directory containing the images
+        self.old_img_dir_entry = cs.CTkEntry(
+            self,
+            width=400
+        )
+        self.old_img_dir_entry.grid(row=2, column=0)
+
+        # pop up explorer to choose the dir containing the images
+        self.old_img_dir_btn = cs.CTkButton(
+            self,
+            text="Choose a directory",
+            height=25,
+            command=self.choose_dir
+        )
+        self.old_img_dir_btn.grid(row=3, column=0)
+
+        # label which explains what the next entry is for
+        self.sort_label = cs.CTkLabel(
+            self,
+            text="Indicater here how the program will sort you photos"
+                 "\n( Default: \"year+s+year+_+month+_+day\" )",
+            font=('Arial', 15)
+        )
+        self.sort_label.grid(row=4, column=0)
+
+        # Place where the help label will be if the sort_help_button is clicked
+        self.rowconfigure(5, weight=0)
+
+        # Make the help label appear or disappear
+        self.sort_help_button = cs.CTkButton(
+            self,
+            text='Help',
+            command=self.help_sort_label_display
+        )
+        self.sort_help_button.grid(row=6, column=0)
+
+        # Entry in which the user inputs the way he wants the program the sort his images
+        self.sort_entry = cs.CTkEntry(self, width=350)
+        self.sort_entry.grid(row=7, column=0)
+
+        # tripath button
+        self.launchpath = cs.CTkButton(
+            self,
+            text="Start sorting at specified path",
+        )
+        self.launchpath.grid(row=8, column=0)
+
+        # tri button
+        self.launch = cs.CTkButton(
+            self,
+            text="Start sorting where the program is"
+        )
+        self.launch.grid(row=9, column=0)
+
+    def choose_dir(self):
+        """
+        Allows the user to choose the directory of the images to sort
+        through a Windows Explorer window
+        """
+        path: str = filedialog.askdirectory(initialdir="./")
+        tmp: cs.StringVar = cs.StringVar()
+        tmp.set(path)
+        self.old_img_dir_entry.configure(textvariable=tmp)
+
+    def help_sort_label_display(self):
+        """Display or destroy the help label"""
+        if not self.help_is_displayed:
+            self.rowconfigure(5, weight=1)
+            self.help_sort_label = cs.CTkLabel(
+                self,
+                text='"year" puts the year the photo was taken, "month" the month, "day" the day.'
+                     '\n "s" allow to create a new folder with the name before'
+                     '\n If you want to put other characters, just put them.',
+                font=('Arial', 15, 'italic')
+            )
+            self.help_sort_label.grid(row=5, column=0)
+            self.help_is_displayed = True
+        else:
+            self.help_sort_label.destroy()
+            self.rowconfigure(5, weight=0)
+            self.help_is_displayed = False
+
+    def warning(self):
+        """
+        The warning function is a function called when the user does not enter a path to the photos.
+        It displays an error message and asks him to enter a path or click on another button.
+        """
+        self.warning_label = cs.CTkLabel(
+            self,
+            text="Warning, please enter a valid path to your images.\nIf you want to start the sorting where the program is, click the other button.",
+            font=("Arial", 10),
+        )
+        self.warning_label.grid(row=10, column=0)
 
 
 if __name__ == '__main__':
